f0c5d08a7416eb1e34e1e6355a4efcf38096a61a
BernardHuxley <108091774+BernardHuxley@users.noreply.github.com>
2023-06-15 19:31:51 +0200

    Add files via upload

---
+++ Winda_z_gui/Winda_z_gui/framework.h
@@ -0,0 +1,27 @@
+﻿// header.h: plik dołączany dla standardowych systemowych plików dołączanych,
+// lub pliki dołączane specyficzne dla projektu
+//
+
+#pragma once
+
+#include "targetver.h"
+//#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
+// Pliki nagłówkowe systemu Windows
+#include <windows.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+
+// Pliki nagłówkowe środowiska uruchomieniowego języka C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include <stdlib.h> //srand, rand
+#include <time.h> //time
+//potrzebne do zapoczątkowania ziarna losującego oraz wygenerowania liczby losowej
+#include <iostream>
+//#include <queue>
+#include<vector>
